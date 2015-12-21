e12238eac84e9752a80b7f0292c78ba4ab232561
Ian Bell <ian.bell@nist.gov>
2015-12-21 10:19:21 -0700

    Remove jQuery pulling code

--- copy_js.py
+++ copy_js.py
@@ -33,23 +33,5 @@ def pull_from_submodules():
         new_name = os.path.join('js', fname)
         shutil.copy2(old_name, new_name)
 
-def pull_jquery():
-    """
-    In order to avoid the dependency on building jquery and jquery UI, 
-    we just download a stable version of jquery
-    """
-    urls = [
-        ('http://code.jquery.com/jquery-1.10.2.js', 'js/jquery.js'),
-        ('http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css','js/jquery-ui-smoothness.css'),
-        ('http://code.jquery.com/ui/1.11.4/jquery-ui.js', 'js/jquery-ui.js'),
-    ]
-    import urllib
-    for url, ofname in urls:
-        
-        handle = urllib.urlopen(url)
-        with open(ofname,'w') as fp:
-            fp.write(handle.read())
-
 if __name__=='__main__':
     pull_from_submodules()
-    #pull_jquery()
